8de2ea8c788144d834c818850f194b547162180b
kopot <0918345a@gmail.com>
2017-04-21 23:04:04 +0300

    Fixes for enable service & rus language

--- run.sh
+++ run.sh
@@ -2,28 +2,25 @@
 ### LOCALE FIX ###
 ##################
 export LC_ALL="en_US.UTF-8"
-apt-get install language-pack-ru
+#apt-get install language-pack-ru
 
 sudo cat >/etc/default/locale <<EOL
-LANG="ru_RU.utf8"
-LANGUAGE="ru_RU:ru"
-LC_CTYPE="ru_RU.utf8"
-LC_NUMERIC="ru_RU.utf8"
-LC_TIME="ru_RU.utf8"
-LC_COLLATE=ru_RU.utf8
-LC_MONETARY="ru_RU.utf8"
-LC_MESSAGES=ru_RU.utf8
-LC_PAPER="ru_RU.utf8"
-LC_NAME="ru_RU.utf8"
-LC_ADDRESS="ru_RU.utf8"
-LC_TELEPHONE="ru_RU.utf8"
-LC_MEASUREMENT="ru_RU.utf8"
-LC_IDENTIFICATION="ru_RU.utf8"
-LC_ALL="ru_RU.utf8"
+LANG="en_US.UTF-8"
+LANGUAGE="en_EN:en"
+LC_CTYPE="en_US.UTF-8"
+LC_NUMERIC="en_US.UTF-8"
+LC_TIME="en_US.UTF-8"
+LC_COLLATE=en_US.UTF-8
+LC_MONETARY="en_US.UTF-8"
+LC_MESSAGES=en_US.UTF-8
+LC_PAPER="en_US.UTF-8"
+LC_NAME="en_US.UTF-8"
+LC_ADDRESS="en_US.UTF-8"
+LC_TELEPHONE="en_US.UTF-8"
+LC_MEASUREMENT="en_US.UTF-8"
+LC_IDENTIFICATION="en_US.UTF-8"
+LC_ALL="en_US.UTF-8"
 EOL
-
-export LC_ALL="ru_RU.utf8"
-export LANG="ru_RU.utf8"
 
 ###sudo update-locale LANG=ru_RU.UTF-8
 
@@ -33,7 +30,7 @@ export LANG="ru_RU.utf8"
 sudo apt-get install libssl-dev
 
 bash <(curl -f -L -sS https://ngxpagespeed.com/install) \
-     --nginx-version latest -y -a '--with-http_ssl_module --with-http_gzip_static_module --with-http_gunzip_module --http-log-path=/var/log/nginx/access.log --error-log-path=/var/log/nginx/error.log --with-stream --with-stream_ssl_module --with-mail --with-mail_ssl_module --with-threads --without-http_browser_module --without-http_geo_module --without-http_limit_req_module --without-http_referer_module --without-http_scgi_module --without-http_split_clients_module --without-http_ssi_module --without-http_userid_module --without-http_uwsgi_module'
+     --force-yes --nginx-version latest -a '--with-http_ssl_module --with-http_gzip_static_module --with-http_gunzip_module --http-log-path=/var/log/nginx/access.log --error-log-path=/var/log/nginx/error.log --with-stream --with-stream_ssl_module --with-mail --with-mail_ssl_module --with-threads --without-http_browser_module --without-http_geo_module --without-http_limit_req_module --without-http_referer_module --without-http_scgi_module --without-http_split_clients_module --without-http_ssi_module --without-http_userid_module --without-http_uwsgi_module'
 
 cp ~/nginx-1.12.0/objs/nginx /usr/sbin/nginx
 
@@ -72,6 +69,7 @@ cat >/webroot/www/index.php <<EOL
     phpinfo();
 EOL
 
+sudo systemctl enable nginx.service
 sudo service nginx start
 
 ###########
@@ -83,7 +81,7 @@ sudo ufw default allow outgoing
 sudo ufw allow 22
 sudo ufw allow 80
 sudo ufw allow 443
-sudo ufw enable
+sudo ufw --force enable
 
 ###############
 ### MONGODB ###
@@ -92,19 +90,21 @@ sudo ufw enable
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
 echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
 sudo apt-get update
-sudo apt-get install -y mongodb-org
+sudo apt-get install -y mongodb-org 
+sudo systemctl enable mongod.service
 sudo service mongod start
 
 ###############
 ### PHP-FPM ###
 ###############
-sudo apt-get install php-fpm php-mongodb
+sudo apt-get install -y php-fpm php-mongodb
+sudo service php7.0-fpm restart
 
 #############
 ### REDIS ###
 #############
 sudo apt-get update
-sudo apt-get install build-essential tcl
+sudo apt-get install -y build-essential tcl
 cd /tmp
 curl -O http://download.redis.io/redis-stable.tar.gz
 tar xzvf redis-stable.tar.gz
@@ -146,8 +146,24 @@ sudo systemctl enable redis
 ####################
 
 sudo apt-get update
-sudo apt-get install nodejs
-sudo apt-get install npm
+sudo apt-get install -y nodejs
+sudo apt-get install -y npm
+
+################
+### COMPOSER ###
+################
+
+cd /tmp
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '669656bab3166a7aff8a7506b8cb2d1c292f042046c5a994c43155c0be6190fa0355160742ab2e1c88d40d5be660b410') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php --install-dir=/usr/sbin --filename=composer
+php -r "unlink('composer-setup.php');"
+
+###############
+### LARAVEL ###
+###############
+
+
 
 exit;
 
